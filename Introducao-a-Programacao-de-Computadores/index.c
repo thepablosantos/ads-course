6d00b9ec7f7f53c9027425f6af91125f0936e196
thepablosantos <iphonepablosantos@gmail.com>
2025-10-15 01:54:56 +0200

    introducao a if e else

---
+++ Introducao-a-Programacao-de-Computadores/index.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+   int idade = 20;
+
+   if (idade >= 18) {
+    printf("Voce e maior de idade");
+   }
+   else {
+    printf("Voce nao e maior de idade");
+   }
+}
